a23cc521fe94c07cf3be4790bcb19fb1e7000f4b
James Shoemaker <james.shoemaker99@gmail.com>
2020-11-01 14:13:14 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+class YourGrade
+
+{
+
+    public static void main(String[] args)
+
+    {
+
+        int projectPoints = 89;
+
+        System.out.print("Your grade for this class is ");
+
+        System.out.print(projectPoints);
+
+        System.out.println("%");
+
+    }
+
+}
